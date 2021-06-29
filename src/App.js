e698425a815ffe30e11473979826a921e336560f
web-galtsov <oksana.galtsov@gmail.com>
2021-06-29 18:36:35 +0300

    Advanced React Hooks (useContext, useReducer)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, {createContext, useContext, useEffect, useReducer, useState} from "react";
 import Login from "./components/Login";
 import Header from "./components/Header";
 import CreatePost from "./components/CreatePost";
 import PostList from "./components/PostList";
+import postReducer from "./reducer";
 
-const functionsCount = new Set();
+export const UserContext = createContext();
+export const PostContext = createContext({
+    posts: []
+});
 
 function App() {
+    const initialPostState = useContext(PostContext);
+    const [state, dispatch] = useReducer(postReducer, initialPostState);
     const [user, setUser] = useState("reed");
-    const [posts, setPosts] = useState([]);
-    const [count, setCount] = useState(0);
+    // const [posts, setPosts] = React.useState([]);
 
     useEffect(() => {
         document.title = user ? `${user}'s Feed` : "Please login";
     }, [user]);
 
-    const handleAddPost = useCallback(
-        newPost => {
-            setPosts([newPost, ...posts]);
-        },
-        [posts]
-    );
-
-    functionsCount.add(handleAddPost);
-    console.log(functionsCount);
+    // const handleAddPost = React.useCallback(
+    //   newPost => {
+    //     setPosts([newPost, ...posts]);
+    //   },
+    //   [posts]
+    // );
 
     if (!user) {
         return <Login setUser={setUser} />;
     }
 
     return (
-        <>
-            <Header user={user} setUser={setUser} />
-            <CreatePost user={user} handleAddPost={handleAddPost} />
-            <PostList posts={posts} />
-            <button onClick={() => setCount(prev => prev + 1)}>{count} +</button>
-        </>
+        <PostContext.Provider value={{ state, dispatch }}>
+            <UserContext.Provider value={user}>
+                <Header user={user} setUser={setUser} />
+                <CreatePost
+                    user={user}
+                    //  handleAddPost={handleAddPost}
+                />
+                <PostList posts={state.posts} />
+            </UserContext.Provider>
+        </PostContext.Provider>
     );
 }
 
