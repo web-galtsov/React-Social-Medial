22f679b0d80e793a1360dbe2a5cb8a0badc04652
web-galtsov <oksana.galtsov@gmail.com>
2021-06-29 17:05:22 +0300

    Building our Social Media App

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {useCallback, useEffect, useState} from "react";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+
+const functionsCount = new Set();
+
+function App() {
+    const [user, setUser] = useState("reed");
+    const [posts, setPosts] = useState([]);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = user ? `${user}'s Feed` : "Please login";
+    }, [user]);
+
+    const handleAddPost = useCallback(
+        newPost => {
+            setPosts([newPost, ...posts]);
+        },
+        [posts]
+    );
+
+    functionsCount.add(handleAddPost);
+    console.log(functionsCount);
+
+    if (!user) {
+        return <Login setUser={setUser} />;
+    }
+
+    return (
+        <>
+            <Header user={user} setUser={setUser} />
+            <CreatePost user={user} handleAddPost={handleAddPost} />
+            <PostList posts={posts} />
+            <button onClick={() => setCount(prev => prev + 1)}>{count} +</button>
+        </>
+    );
+}
+
+export default App;
